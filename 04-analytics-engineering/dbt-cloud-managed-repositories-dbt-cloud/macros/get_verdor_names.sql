1a41ddc0f99c8ef7baea45d6d380000930d2207c
Elyas Randi Renaldi <134897021+onecme@users.noreply.github.com>
2026-02-13 13:31:16 +0700

    Delete 04-analytics-engineering/dbt-cloud-managed-repositories-dbt-cloud directory

--- 04-analytics-engineering/dbt-cloud-managed-repositories-dbt-cloud/macros/get_verdor_names.sql
+++
@@ -1,9 +0,0 @@
-{% macro get_vendor_names(vendorid) %}
-
-case 
-    when {{vendorid}} = 1 then 'Creative Mobile Technologies, LLC'
-    when {{vendorid}} = 2 then 'VeriFone Inc.'
-    else 'Unknown Vendor'
-end 
-
-{% endmacro %}
--- 04-analytics-engineering/dbt-cloud-managed-repositories-dbt-cloud/models/marts/fct_trips.sql
+++
@@ -1,9 +0,0 @@
-with trips as (
-
-    select *
-    from {{ ref('int_trips_unioned') }}
-
-)
-
-select *
-from trips
